682f4b723c75209f2c3b3e913bbcca383ed27e72
Harumi <69336021+H4RU77@users.noreply.github.com>
2024-09-16 21:20:52 -0700

    Update main2.c

--- main2.c
+++ main2.c
@@ -31,7 +31,7 @@ void push(Pila *p, char val) {
 
 char pop(Pila *p) {
     if (estaVacia(p)) {
-        printf("Error: Pila vacía\n");
+        printf("Error: Pila vacÃ­a\n");
         return '\0';
     }
     return p->items[(p->top)--];
@@ -67,64 +67,9 @@ void infijoAPostfijo(char exp[], char resultado[]) {
     int i, j = 0;
 
     for (i = 0; exp[i] != '\0'; i++) {
-        if (isdigit(exp[i])) {
-            resultado[j++] = exp[i]; // Número directamente al resultado
-        } else if (esOperador(exp[i])) {
-            if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{') {
-                push(&pila, exp[i]);
-            } else if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}') {
-                char open;
-                if (exp[i] == ')') open = '(';
-                else if (exp[i] == ']') open = '[';
-                else open = '{';
-
-                while (!estaVacia(&pila) && tope(&pila) != open) {
-                    resultado[j++] = pop(&pila);
-                }
-                pop(&pila); // Eliminar '(' o '[' o '{'
-            } else {
-                while (!estaVacia(&pila) && precedencia(tope(&pila)) >= precedencia(exp[i])) {
-                    resultado[j++] = pop(&pila);
-                }
-                push(&pila, exp[i]);
-            }
-        }
     }
-    while (!estaVacia(&pila)) {
-        resultado[j++] = pop(&pila);
-    }
-    resultado[j] = '\0'; // Cerrar la cadena
-}
-
-int evaluarPostfijo(char postfijo[]) {
-    Pila pila;
-    inicializarPila(&pila);
-    int i, op1, op2;
-
-    for (i = 0; postfijo[i] != '\0'; i++) {
-        if (isdigit(postfijo[i])) {
-            push(&pila, postfijo[i] - '0'); // Convertir carácter a número
-        } else {
-            op2 = pop(&pila);
-            op1 = pop(&pila);
-            switch (postfijo[i]) {
-                case '+': push(&pila, op1 + op2); break;
-                case '-': push(&pila, op1 - op2); break;
-                case '*': push(&pila, op1 * op2); break;
-                case '/': push(&pila, op1 / op2); break;
-            }
-        }
-    }
-    return pop(&pila);
 }
 
 int main() {
-    char expresion[] = "{20*8} - [9/3] + (3*3)";
-    char postfijo[MAX];
-
-    infijoAPostfijo(expresion, postfijo);
-    printf("Expresión postfija: %s\n", postfijo);
-    printf("Resultado: %d\n", evaluarPostfijo(postfijo));
-
-    return 0;
+    
 }
